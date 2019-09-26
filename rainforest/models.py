ef897a48aca4eeff3bdf684cc1015fe4512e20b9
ilyana19 <aries_2152@hotmail.com>
2019-09-25 21:33:09 -0400

    created product model migration

---
+++ rainforest/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Product(models.Model):
+  name = models.CharField(max_length=255)
+  description = models.TextField()
+  price_in_cents = models.IntegerField()
+
+  def __str__(self):
+    return self.name
