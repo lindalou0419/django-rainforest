79cc787b31d4e3485b8187c40de89a2e1ac4e010
ilyana19 <aries_2152@hotmail.com>
2019-09-25 22:52:50 -0400

    seeded db with fixture and then created superuser admin to double check

--- rainforest/migrations/0001_initial.py
+++ rainforest/migrations/0001_initial.py
@@ -5,19 +5,19 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
+  initial = True
 
-    dependencies = [
-    ]
+  dependencies = [
+  ]
 
-    operations = [
-        migrations.CreateModel(
-            name='Product',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
-                ('description', models.TextField()),
-                ('price_in_cents', models.IntegerField()),
-            ],
-        ),
-    ]
+  operations = [
+    migrations.CreateModel(
+      name='Product',
+      fields=[
+        ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        ('name', models.CharField(max_length=255)),
+        ('description', models.TextField()),
+        ('price_in_cents', models.IntegerField()),
+      ],
+  ),
+  ]
